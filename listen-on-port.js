73d66d48064a9f2c642ea891635146e91417d97e
Natalie Carey <natalie@nowprototype.it>
2024-09-09 00:17:30 +0100

    Added more granular metrics.

--- listen-on-port.js
+++ listen-on-port.js
@@ -1,23 +1,25 @@
 const timeToListenOnPort = Date.now() - global.initialStartTime
 
-process.on('disconnect', () => {
-  process.exit()
-})
-
 // npm dependencies
 const config = require('./lib/config.js').getConfig(null, false)
+const timeToConfigLoaded = Date.now() - global.initialStartTime
 const server = require('./server.js')
-const events = require('./lib/dev-server/dev-server-events')
-const eventTypes = require('./lib/dev-server/dev-server-event-types')
-const { monitorEventLoop } = require('./lib/utils')
-
-monitorEventLoop('prototype')
+const timeToServerLoaded = Date.now() - global.initialStartTime
 
 if (config.isProduction) {
   server.listen(config.port)
   const timeToListening = Date.now() - global.initialStartTime
-  console.log(`Your prototype is running on port ${config.port}, took ${timeToListening}ms to start (${timeToListenOnPort}ms to listen on port)`)
+  console.log(`Your prototype is running on port ${config.port}, took ${timeToListening}ms to start (${timeToListenOnPort}ms to listen on port, ${timeToConfigLoaded}ms to load config, ${timeToServerLoaded}ms to load server)`)
 } else {
+  process.on('disconnect', () => {
+    process.exit()
+  })
+
+  const events = require('./lib/dev-server/dev-server-events')
+  const eventTypes = require('./lib/dev-server/dev-server-event-types')
+  const { monitorEventLoop } = require('./lib/utils')
+
+  monitorEventLoop('prototype')
   const listener = server.listen(() => {
     events.emitExternal(eventTypes.KIT_STARTED, {
       port: listener.address().port
