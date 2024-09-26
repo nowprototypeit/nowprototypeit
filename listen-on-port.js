6a6e3f9382418e6627b25b2852e4ca42edf79c48
Natalie Carey <natalie@nowprototype.it>
2024-09-26 22:28:00 +0100

    Hosting improvements (#28)

--- listen-on-port.js
+++ listen-on-port.js
@@ -1,20 +1,19 @@
-process.on('disconnect', () => {
-  process.exit()
-})
-
-// npm dependencies
 const config = require('./lib/config.js').getConfig(null, false)
 const server = require('./server.js')
-const events = require('./lib/dev-server/dev-server-events')
-const eventTypes = require('./lib/dev-server/dev-server-event-types')
-const { monitorEventLoop } = require('./lib/utils')
-
-monitorEventLoop('prototype')
 
 if (config.isProduction) {
   server.listen(config.port)
-  console.log('Your prototype is running on port', config.port)
+  console.log(`Your prototype is running on port ${config.port}`)
 } else {
+  process.on('disconnect', () => {
+    process.exit()
+  })
+
+  const events = require('./lib/dev-server/dev-server-events')
+  const eventTypes = require('./lib/dev-server/dev-server-event-types')
+  const { monitorEventLoop } = require('./lib/utils')
+
+  monitorEventLoop('prototype')
   const listener = server.listen(() => {
     events.emitExternal(eventTypes.KIT_STARTED, {
       port: listener.address().port
